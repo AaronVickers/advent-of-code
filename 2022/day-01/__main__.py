237a49c0a58634f733f881b9bb919db8075dee3b
Aaron Vickers <aaron_vickers@hotmail.co.uk>
2022-12-08 21:58:59 +0000

    Day 01 solution

--- 2022/day-01/__main__.py
+++ 2022/day-01/__main__.py
@@ -1,8 +1,49 @@
 def part1():
-    file = open("input.txt")
+    elves = [0]
+    index = 0
 
-    for line in file:
-        pass
+    with open("input.txt") as file:
+        for line in file:
+            try:
+                elves[index] += int(line)
+            except:
+                index += 1
+                elves.append(0)
+    
+    print("Part 1: {0}".format(max(elves)))
+
+def part2():
+    numTopElves = 3
+    topElves = [0] * numTopElves
+    current = 0
+    
+    def tryReplaceTopElves(calories):
+        replaced = False
+        previous = 0
+
+        for i in range(numTopElves):
+            if replaced:
+                currentPrevious = previous
+                previous = topElves[i]
+                topElves[i] = currentPrevious
+            elif calories > topElves[i]:
+                replaced = True
+
+                previous = topElves[i]
+                topElves[i] = calories
+
+    with open("input.txt") as file:
+        for line in file:
+            try:
+                current += int(line)
+            except:
+                tryReplaceTopElves(current)
+                current = 0
+        
+        tryReplaceTopElves(current)
+    
+    print("Part 2: {0}".format(sum(topElves)))
 
 if __name__ == "__main__":
     part1()
+    part2()
