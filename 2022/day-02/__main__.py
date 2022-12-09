8ede3e39c88d6b4515e563a0ed5b20dba7ab492f
Aaron Vickers <aaron_vickers@hotmail.co.uk>
2022-12-09 10:28:33 +0000

    Day 02 solution

--- 2022/day-02/__main__.py
+++ 2022/day-02/__main__.py
@@ -18,5 +18,29 @@ def part1():
     
     print("Part 1: {0}".format(totalScore))
 
+def part2():
+    totalScore = 0
+
+    lowerOrd = ord('A')
+    upperOrd = ord('X')
+
+    def wrapMove(move):
+        return 1 + (move - 1) % (4 - 1)
+
+    with open("input.txt") as file:
+        for line in file:
+            round = line.strip().split(" ")
+            round[0] = ord(round[0]) - lowerOrd + 1
+            round[1] = ord(round[1]) - upperOrd + 1
+
+            move = wrapMove(round[0]-1) if round[1] == 1 else round[0] if round[1] == 2 else wrapMove(round[0]+1)
+
+            score = move + (0 if round[1] == 1 else 3 if round[1] == 2 else 6)
+
+            totalScore += score
+    
+    print("Part 2: {0}".format(totalScore))
+
 if __name__ == "__main__":
     part1()
+    part2()
